bb2cc3ebfd3de5fa41cd13bb1b3be61256e118ec
jfriel <james.friel4@gmail.com>
2016-05-30 09:18:38 +0100

    moved the tokenization into a seperate function. Having trouble with nltk, will look into this.

---
+++ parser/getTokens.py
@@ -0,0 +1,12 @@
+from nltk import tokenize, word_tokenize
+
+def tokens(document):
+    tokenized_sentences  = []
+    sentences = tokenize.sent_tokenize(document)
+    for sentence in sentences:
+        tokenized_sentence = word_tokenize(sentence)
+        tokenized_sentences.append(tokenized_sentences)
+    return tokenized_sentences
+
+
+print tokens("Hello")
---
+++ parser/tagparser.py
@@ -0,0 +1,23 @@
+
+
+def tag_remover(article):
+"""
+===Tag Types===
+!doctype
+reuters
+date
+topics
+places
+people
+orgs
+exchanges
+companies
+unknown
+title
+dateline
+body
+========
+reuters is the document container
+"""
+
+
