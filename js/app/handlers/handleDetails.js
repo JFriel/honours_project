744eaf6dd8935565dbdb413bc2126b38ba88d92d
jfriel <james.friel4@gmail.com>
2016-06-25 10:03:57 +0100

    started to add the filtering

---
+++ js/app/handlers/handleDetails.js
@@ -0,0 +1,14 @@
+
+
+const weekday=['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+const months = ['january','february','march','april','may','june','july','august','september','october','november','december']
+const timescale = ['week','month','year','day','days']
+
+const extractData = (data) => {
+    console.log(data.date);
+}
+
+
+module.exports = {
+    extract: extractData
+}
--- js/server.js
+++ js/server.js
@@ -3,11 +3,14 @@ const express = require('express');
 const natural = require('natural');
 const routes = require('./app/routes/routes');
 const generate = require('./app/handlers/getInfo')
+const details = require('./app/handlers/handleDetails');
 const HTML5Tokenizer = require('simple-html-tokenizer');
 const app = express();
 
 
 app.get('/', generate.generateText);
+//app.get('/details',details.extract(generate.generateText) );
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
