6f57c2056d5c6bdfaa23213ff8769822254cc234
jfriel <james.friel4@gmail.com>
2016-11-17 15:56:14 +0000

    i think this classifier works

--- class.py
+++ class.py
@@ -5,4 +5,4 @@ with open('data/train.csv', 'r') as fp:
     cl = NaiveBayesClassifier(fp, format="csv")
 
 
-print cl.classify("This is an amazing library!")
+print cl.classify("International Book Year")
