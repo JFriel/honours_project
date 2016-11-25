2b662e38b1e6a2c325164e0cdf9deb0fe5b6de5d
jfriel <james.friel4@gmail.com>
2016-11-25 15:15:42 +0000

    start on setup1

---
+++ app/analytics/featureExtraction.py
@@ -0,0 +1,34 @@
+import collections, re
+from sklearn.feature_extraction.text import CountVectorizer
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
+import operator
+def preprocessor(data):
+    return " ".join([SnowballStemmer("english").stem(word) for word in data.split()])
+
+def getWords(sentence):
+    returnWords = []
+    words = re.findall(r'\w+', sentence.lower())
+    for w in words:
+        if w not in stopwords.words('english'):
+            returnWords.append(w)
+    return returnWords
+
+def bagOfWords(texts):
+    #vectorizer = CountVectorizer(preprocessor=preprocessor).fit(texts)
+    #bagOfWords = vectorizer.vocabulary_
+    #sorted_bagOfWords = sorted(bagOfWords.items(), key=operator.itemgetter(1))
+    #return sorted_bagOfWords
+
+    bagsofwords = [ collections.Counter(getWords(txt)) for txt in texts]
+    #bagsofwords = [w for w in bagsofwords if not w in stopwords.words("english")]
+    filtered_words = [word for word in bagsofwords if word not in stopwords.words('english')]
+    sumbags = sum(filtered_words, collections.Counter())
+    return dict(sumbags)
+
+
+def featureExtraction(sentences):
+    bow = bagOfWords(sentences)
+    return bow
+
+
---
+++ setup1.py
@@ -0,0 +1,68 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import app.parser.getData as importArticles
+import app.parser.articleRetrieval.getArticles as getContent
+import app.parser.sentences as sent
+import app.parser.getChunks as gc
+import app.analytics.tag as tag
+import app.parser.articleRetrieval.wikipediaParse as wp
+import app.analytics.featureExtraction as fe
+from sklearn import tree, feature_extraction
+import numpy as np
+
+articles=importArticles.getData()
+
+singleSets=[]
+doubleSets = []
+#A
+for article in articles[0:10]:
+    try:
+        chunks = gc.getChunks(article[1])
+        tags =  tag.getTags(article[1],chunks)
+        if tags == []:
+            continue # check this is right. go to next itteration
+        """The Stanford Open IE tags"""
+        subject = tags['subject']
+        relation = tags['relation']
+        objects = tags['object']
+        objects = objects.split()
+
+        content = wp.getArticle(subject)
+        rawSentences = sent.getSentences(content)
+        sentences= fe.featureExtraction(rawSentences)
+        print sentences
+        print '\n\n\n'
+        singleSets.append({'title':article[1], 'sentences':sentences, 'year':article[0]})
+    except:
+        pass
+#B
+
+for i in range(len(singleSets)):
+    for j in range(i+1, len(singleSets)):
+        if(singleSets[i]['year'] < singleSets[j]['year']):
+            b = 1
+        else:
+            b = 0
+        doubleSets.append({'title1':singleSets[i]['title'],'sentences1': singleSets[i]['sentences'],'title2':singleSets[j]['title'],'sentences2': singleSets[j]['sentences'], 'year':b})
+
+#C
+bools = []
+features = []
+
+for item in doubleSets:
+    bools.append(item['year'])
+    features.append( [feature_extraction.DictVectorizer(item['sentences1']), feature_extraction.DictVectorizer(item['sentences2'])])
+
+
+print type(features)
+print type(features[0])
+print type(features[0][0])
+X = np.array(features)
+Y = np.array(bools)
+
+#Need to let it take i a dict or something)
+clf = tree.DecisionTreeClassifier()
+clf=clf.fit(X,Y)
+
